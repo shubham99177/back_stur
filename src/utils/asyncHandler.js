80f067cbee29b72cdd93ceda9360c6f5a9727c83
shubham99177 <shubhamrathoree123@gmail.com>
2024-09-26 11:45:08 +0530

    utils code push

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,19 @@
-// const asyncHandler = ()=>{}
+const asyncHandler = (requesthandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err))
+
+    }
+}
 
 export { asyncHandler };
 
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(req, res, next);
-        
-        
-    } catch (error) {
-        res.status(err.code || 500).json({ message: err.message });
-    }
-};
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
