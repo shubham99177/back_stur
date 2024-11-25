0337de00e40914e891a972ebae0ef0b2d6130ee8
shubham99177 <shubhamrathoree123@gmail.com>
2024-11-25 22:13:15 +0530

    test

--- src/index.js
+++ src/index.js
@@ -11,11 +11,15 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
+      console.log(`Server is running at port: ${process.env.PORT || 8000} `);
     });
   })
   .catch((err) => {
     console.log("MONGO DB CONNECTION FAILED!!!", err);
+  });
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
   });
 
 
